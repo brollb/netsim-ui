b9215207f2b830ac1fae2197806782cc72125e09
Brian Broll <brian.broll@gmail.com>
2015-03-20 12:40:12 -0500

    Added basic settings for webgme

---
+++ config.js
@@ -0,0 +1,16 @@
+'use strict';
+var config = require('webgme/config/config.default'),
+    validateConfig = require('webgme/config/validator');
+
+// Overwrite options as needed
+config.server.port = 8080;
+config.mongo.uri = 'mongodb://127.0.0.1:27017/netsim';
+
+// Customize Visualizers
+config.visualization.visualizerDescriptors = ['./Visualizers.json'];
+
+// Plugin paths
+config.plugin.basePaths.push('Plugins');
+
+validateConfig(config);
+module.exports = config;
